2de0ed475c160e69b42177fa00364705ba01b34e
Ota Fejfar <ota.fejfar@gmail.com>
2025-10-29 17:55:34 +0100

    chore: Adding RGB LEDPalette variant LEDPaletteRGB
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

---
+++ palette/LEDPaletteRGB.cpp
@@ -0,0 +1,40 @@
+/* LEDPaletteRGB - Managing the Palette of RGB LED colors
+ * Copyright (C) 2025 DygmaLabs, S. L.
+ *
+ * The MIT License (MIT)
+ * Copyright © 2025 <copyright holders>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the “Software”), to deal in the
+ * Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "LEDPaletteRGB.h"
+
+result_t LEDPaletteRGB::init( void )
+{
+    result_t result = RESULT_ERR;
+
+    p_color_palette = &color_palette;
+    color_palette_size = sizeof( color_palette );
+    color_size = sizeof( color_t );
+
+    result = LEDPalette::init();
+    EXIT_IF_ERR( result, "LEDPalette::init failed" );
+
+_EXIT:
+    return result;
+}
