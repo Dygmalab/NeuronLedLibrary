482a993a5cb269e1760cc56269b6c733be80176d
Ota Fejfar <ota.fejfar@gmail.com>
2025-10-30 12:50:42 +0100

    chore: Moving the LED effect sources to effects folder
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

---
+++ effects/LEDEffect-Stalker-Defy.h
@@ -0,0 +1,36 @@
+/* LEDEffect-Stalker-Defy - Solid color LED effects for keyscanner sides.
+ * Copyright (C) 2017  Keyboard.io, Inc.
+ * Copyright (C) 2023, 2024  DygmaLabs, S. L.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "LEDEffect.h"
+#include "LedModeSerializable-Stalker.h"
+#include "LedModeCommunication.h"
+
+class LEDStalkerDefy : public LEDEffect,
+                       public LedModeCommunication {
+  public:
+    LEDStalkerDefy() : LEDEffect( LED_EFFECT_TYPE_STALKER )
+    {
+        led_mode.base_settings.delay_ms = 50;
+    }
+    LedModeSerializable_Stalker &led_mode = ledModeSerializableStalker;
+
+    void activate() final;
+};
+
+extern class LEDStalkerDefy LEDStalkerDefy;
